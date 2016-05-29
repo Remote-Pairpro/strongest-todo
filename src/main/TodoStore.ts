e7046e5a991551d4322a069678a6a71759b6ee2a
Hidari0415 <ika5140@gmail.com>
2016-05-29 22:09:58 +0900

    プリントデバッグを削除

--- src/main/TodoStore.ts
+++ src/main/TodoStore.ts
@@ -13,25 +13,20 @@ export default class TodoStore {
     public localSave: boolean = true;
 
     public load(): Todo[] {
-        console.log("ロード手前。");
         let loaded: Todo[] = [];
         let json = null;
         if (this.localSave) {
             json = localStorage.getItem(TodoStore.KEY);
         }
         if (json !== null) {
-            console.log("ロード後。");
-            console.log("json:" + json);
             loaded = JSON.parse(json);
         }
         return loaded;
     }
 
     public save(todos: Todo[]) {
-        console.log("セーブ前、件数" + todos.length);
         // JSON文字列にしてlocalStrage保存。
         const json: string = JSON.stringify(todos);
-        console.log("json:" + json);
         if (this.localSave) {
             localStorage.setItem(TodoStore.KEY, json);
         }
